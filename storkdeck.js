8cd2e1f5bd673babbda2f95301da1a52fe489f1e
Password Reset <abuse@localhost>
2014-12-14 14:32:50 +0000

    Various selector fixes

--- storkdeck.js
+++ storkdeck.js
@@ -1,7 +1,9 @@
-var css = 'ul.tweet-actions, ul.tweet-detail-actions{display:none}'    +
-          'button.tweet-button{display:none}' +
-          'button.js-follow-button' +
-          'div.js-detail-view-inline.detail-view-inline.item-box-full-bleed.padding-axl.scroll-conversation{display:none}';
+var css = 'ul.tweet-actions, ul.tweet-detail-actions{display:none}' +
+          'button.btn-compose{display:none}' +
+          'button.js-follow-button{display:none}' +
+          'div.inline-reply{display:none}' +
+          'div.js-inline-reply.inline-reply.is-inline-active{display:none}' + 
+          'div.js-detail-view-inline.detail-view-inline{display:none}';
 
 var head  = document.head,
     style = document.createElement('style');
